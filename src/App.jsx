0c5f4770dc3b553e169e21016c62dfc2d065d1b3
Dylan D <dore.dylan31@gmail.com>
2023-09-21 18:09:26 +0200

    Akind card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import { AkindNavbar } from './components/AkindNavbar/AkindNavbar'
+import { AkindCard } from './components/Cards/AkindCard/AkindCard'
+import { AkindNavbar } from './components/Navbar/AkindNavbar/AkindNavbar'
 
 function App() {
 
   return (
   <div className="main_container">
     <AkindNavbar />
+    <AkindCard />
   </div>
   )
 }
