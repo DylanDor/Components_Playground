4040744796a2de6aff3de90ddcdf934fa912e4b5
Dylan D <dore.dylan31@gmail.com>
2023-09-21 15:17:05 +0200

    init project & AkindNavbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { AkindNavbar } from './components/AkindNavbar/AkindNavbar'
+
+function App() {
+
+  return (
+  <div className="main_container">
+    <AkindNavbar />
+  </div>
+  )
+}
+
+export default App
